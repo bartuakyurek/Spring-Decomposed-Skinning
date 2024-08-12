1c9a657b2f03d9617d09f43ee7a3f94e13a3f495
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2024-08-12 15:08:35 +0300

    refactor

--- optimize_fit_main.py
+++ optimize_fit_main.py
@@ -16,25 +16,15 @@ import os
 import numpy as np
 import torch
 import meshplot 
+import matplotlib.pyplot as plt
 
 from smpl_torch_batch import SMPLModel
-from dmpl_torch_batch import DMPLModel
 
-
-from skeleton_data import get_smpl_skeleton
-
-# TODO: use './data/female_bpts2dbs.pt' 
-# TODO: turn shuffle on for training dataset
-# TODO: create validation and test splits as well
-bpts2dbs_data = torch.load('./data/50004_dataset.pt')
-training_data = bpts2dbs_data 
-data_loader = torch.utils.data.DataLoader(training_data, batch_size=1, shuffle=False)
-
-# TODO: search for available devices (in colab demo)
-# TODO: gather every path variable in a path.py file, e.g. SMPL_PATH = './path/to/smpl'
 device = "cpu"
+
+training_data = torch.load('./data/50004_dataset.pt')
+data_loader = torch.utils.data.DataLoader(training_data, batch_size=1, shuffle=False)
 smpl_model = SMPLModel(device=device, model_path='./body_models/smpl/female/model.pkl')
-#dmpl_model = DMPLModel(device=device, model_path='./body_models/dmpls/male/model.npz')
 
 for data in data_loader:
    beta_pose_trans_seq = data[0].squeeze().type(torch.float64)
@@ -43,16 +33,38 @@ for data in data_loader:
    trans = beta_pose_trans_seq[:,82:] 
    
    target_verts = data[1].squeeze()
-   smpl_verts, joints, LBS_T = smpl_model(betas, pose, trans, return_T=True)
-   
-   
-   SELECTED_FRAME = 150
-   joint_locations = joints[SELECTED_FRAME].numpy()
-   kintree = get_smpl_skeleton()
-   
    # -----------------------------------------------------------------------
    
+   init_betas = torch.clone(betas)
    
+   betas_param = torch.nn.Parameter(betas, requires_grad=True)
+   pose_param = torch.nn.Parameter(pose, requires_grad=True)
+   trans_param = torch.nn.Parameter(trans, requires_grad=True)
+
+   n_iters = 70
+   learning_rate = 1e-6
+    
+   criterion = torch.nn.MSELoss()
+   optimizer = torch.optim.SGD([betas_param, pose_param, trans_param], learning_rate)
+
+   smpl_model.eval() # ????
+   train_loss = []
+   for i in range(n_iters):
+       
+       smpl_verts, joints = smpl_model(betas_param, pose_param, trans_param)
+       loss = criterion(target_verts, smpl_verts)
+       
+       loss.backward()
+       optimizer.step()
+       optimizer.zero_grad()
+       
+       train_loss.append(loss.item())
+       print(f"loss ({i}): {loss.item()}")
+       
+       #print(torch.sum(tmp_betas - betas_param))
+    
+    
+   plt.plot(train_loss)
    
    # -----------------------------------------------------------------------
    # Break from for loop since we only wanna visualize one mesh rn
