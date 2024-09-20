b470e505d2ac05fb0e1c3024a865cbae1ee12274
Bartu <bartuakyurek@outlook.com>
2024-09-20 11:38:34 +0300

    refactor

--- main_manual_springs.py
+++ main_manual_springs.py
@@ -53,9 +53,9 @@ for data in data_loader:
    f = smpl_model.faces
    
    ### Manuel Spring Data
-   spring_rest_locations = np.array([[0.4, 0.2, 0.0],
-                                     [0.1, 0.2, 0.3]])
-   spring_parents = [9, 9] #[13, 15]
+   spring_rest_locations = np.array([[0.0, 0.0, 1.0],
+                                     [0.0, 0.0, 1.0]])
+   spring_parents = [13, 12]
    
    ## Create viewer canvas and add a mesh (only single mesh is allowed rn)
    single_mesh_viewer = Viewer()
