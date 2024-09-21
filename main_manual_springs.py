d1b5567756690aa844be4b45a6fdd44553af7cb8
Bartu <bartuakyurek@outlook.com>
2024-09-21 11:52:58 +0300

    added open3d

--- main_manual_springs.py
+++ main_manual_springs.py
@@ -25,53 +25,99 @@ from skeleton_data import get_smpl_skeleton
 from viewer import Viewer
 from colors import SMPL_JOINT_COLORS
 
-bpts2dbs_data = torch.load('./data/50004_dataset.pt')
-training_data = bpts2dbs_data 
+training_data = torch.load('./data/50004_dataset.pt')
 data_loader = torch.utils.data.DataLoader(training_data, batch_size=1, shuffle=False)
 
 device = "cpu"
 smpl_model = SMPLModel(device=device, model_path='./body_models/smpl/female/model.pkl')
+kintree = get_smpl_skeleton()
+F = smpl_model.faces
 
 for data in data_loader:
    beta_pose_trans_seq = data[0].squeeze().type(torch.float64)
-   betas = beta_pose_trans_seq[:,:10]
-   pose = beta_pose_trans_seq[:,10:82]
-   trans = beta_pose_trans_seq[:,82:] 
-   
+   betas, pose, trans = beta_pose_trans_seq[:,:10], beta_pose_trans_seq[:,10:82], beta_pose_trans_seq[:,82:] 
    target_verts = data[1].squeeze()
    smpl_verts, joints = smpl_model(betas, pose, trans)
-   
-   
-   SELECTED_FRAME = 150
-   joint_locations = joints[SELECTED_FRAME].numpy()
-   kintree = get_smpl_skeleton()
-   
-   # -----------------------------------------------------------------------
-   
-   v = smpl_verts.detach().cpu().numpy()#[SELECTED_FRAME]
-   j = joints.detach().cpu().numpy()#[SELECTED_FRAME]
-   f = smpl_model.faces
-   
-   ### Manuel Spring Data 
-   spring_rest_locations = np.array([[0.0, 0.0, 1.0],
-                                     [0.0, 0.0, 1.0]])
-   spring_parents = [13, 12]
-   
-   ## Create viewer canvas and add a mesh (only single mesh is allowed rn)
-   single_mesh_viewer = Viewer()
-   single_mesh_viewer.set_mesh_animation(v, f)
-   single_mesh_viewer.set_mesh_opacity(0.6)
-   
-   ## Skeleton and springs
-   single_mesh_viewer.set_skeletal_animation(j, kintree)
-   single_mesh_viewer.set_spring_rig(spring_parents, kintree)
-   
-   ## Run animation
-   single_mesh_viewer.run_animation() #, jpg_dir=jpg_path+"{}.jpg")
-
-   
-   # -----------------------------------------------------------------------
-   # Break from for loop since we only wanna visualize one mesh rn
    break
+   
+V = smpl_verts.detach().cpu().numpy()#[SELECTED_FRAME]
+J = joints.detach().cpu().numpy()#[SELECTED_FRAME]
+n_frames, n_verts, n_dims = target_verts.shape
+
+### Manual Spring Data 
+P = np.array([
+                [0.5, 3.0, 0.5],
+                [2.0, 3.0, 0.0],
+                [1.0, 2.0, 1.0],
+                [1.0, 1.0, 0.0],
+                [0.0, 1.0, 0.0],
+            ])
+S = np.array([
+                [0, 2],
+                [1, 2],
+                [2, 4],
+                [2, 3],
+                [3, 4]
+            ])
+   
+import open3d as o3d
+
+
+mesh_faces = o3d.utility.Vector3iVector(F)
+mesh_verts = o3d.utility.Vector3dVector(V[0])
+mesh = o3d.geometry.TriangleMesh(mesh_verts, mesh_faces)
+mesh.compute_vertex_normals()
+
+
+def test_animation_callback():
+    oct = o3d.geometry.TriangleMesh.create_octahedron()
+    def cb_test(vis, time):
+        print('in cb_test, time =', time)
+        oct.paint_uniform_color(np.random.rand(3))
+        
+    o3d.visualization.draw({'name': 'oct', 'geometry': oct},
+                           on_animation_frame = cb_test,
+                           animation_time_step = 1 / 60,
+                           animation_duration = 1000000,
+                           show_ui=True)
+    
+test_animation_callback()
+
+"""
+def custom_draw_geometry_with_rotation(pcd):
+   
+    def rotate_view(vis):
+        ctr = vis.get_view_control()
+        ctr.rotate(10.0, 0.0)
+        return False
+
+    o3d.visualization.draw_geometries_with_animation_callback([pcd],
+                                                              rotate_view)
+custom_draw_geometry_with_rotation(mesh)
+"""
+
+
+
+
+"""
+
+vis = o3d.visualization.Visualizer()
+vis.create_window()
+
+# geometry is the point cloud used in your animation
+geometry = o3d.geometry.TriangleMesh()
+#geometry.triangles = F
+#geometry.vertices = V[0]
+vis.add_geometry(geometry)
+
+
+for i in range(num_frames):
+    # now modify the points of your geometry
+    # you can use whatever method suits you best, this is just an example
+    geometry.points = pcd_list[i].points
+    vis.update_geometry(geometry)
+    vis.poll_events()
+    vis.update_renderer()
+"""
 
      
