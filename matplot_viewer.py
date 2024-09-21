aae63875ce9e0359cb45406f46e244142e83cf69
Bartu <bartuakyurek@outlook.com>
2024-09-21 19:06:23 +0300

    failed matplotlib animation

--- matplot_viewer.py
+++ matplot_viewer.py
@@ -7,13 +7,39 @@ Created on Sat Sep 21 12:57:13 2024
 """
 
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from viewer import Viewer
+
 
 class Matplot_Viewer(Viewer):
     def __init__(self):
         super().__init__()
-
+        
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(projection='3d')
+        
+        self.xdata, self.ydata = [], []
+        self.ln = self.ax.plot([], [], 'ro')
+        
+    def init(self):
+        import numpy as np
+        self.ax.set_xlim(0, 2*np.pi)
+        self.ax.set_ylim(-1, 1)
+        return self.ln
+            
+    
     def run(self):
-        print("Is animating? ", self.is_animating)
+        import numpy as np
+        ani = FuncAnimation(self.fig, 
+                            self.update, 
+                            frames=np.linspace(0, 2*np.pi, 128),
+                            init_func=self.init, blit=True)
         plt.show()
+        
+
+    def update(self, frame):
+        self.xdata.append(frame)
+        self.ydata.append(np.sin(frame))
+        self.ln.set_data(xdata, ydata)
+        return ln
