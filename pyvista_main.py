4a9be7b511e1b9a821eca183d756b2d93058085f
bartuakyurek <bartuakyurek@outlook.com>
2024-09-30 11:42:50 +0300

    Add spring locations manually
    Add springs as we did in earlier commits using spring.py interface, however at this point I realize we shouldn't add the springs in this way. Instead we need to create individual mass particles first. Then, add connected particles and rest offsets for each that will represent the springs.

--- pyvista_main.py
+++ pyvista_main.py
@@ -43,14 +43,8 @@ n_frames, n_verts, n_dims = target_verts.shape
 import numpy as np
 import pyvista as pv
 
+from spring import Spring
 from pyvista_render_tools import add_skeleton, add_mesh
-
-## TODO: Add springs
-
-
-
-## TODO: Simulate springs w.r.t. rigid motion (and save simulation)
-
 
 
 ## TODO: Render the baked simulation using Pyvista
@@ -69,6 +63,31 @@ plotter.open_gif("./results/sample.gif")
 n_frames = 200 
 n_repeats = 5
 for _ in range(n_repeats):
+    
+    ## TODO: Add springs
+
+    spring_coords = np.array([
+                                [0.5, 1.0, 0.5],
+                                ])
+
+    # This is the same as adding another mass and connecting it to the first
+    # mass with a spring vector. 
+    # TODO: maybe we can change the implementation
+    # for a more intuitive mass-spring lattice creation
+    spring_rest_vectors = np.array([
+                                    [1.0, 0.0, 0.0],
+                                    ])
+
+    # TODO: could we create a shallow copy array, to combine SMPL joints plus spring coordinates? 
+    spring_instances = []
+    spring_coords_bake = []
+
+    # Loop create springs 
+    n_springs = 1
+    for i in range(n_springs):
+       pass
+    ## TODO: Simulate springs w.r.t. rigid motion (and save simulation)
+    
     for frame in range(n_frames-1):
         
         pts = J[frame].copy()
