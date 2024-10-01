6cf5980e26fa4f552d045850e24abd55c3df7f1a
bartuakyurek <bartuakyurek@outlook.com>
2024-10-01 10:40:55 +0300

    Change mass dict to array
    Getting masses in the system from a dictionary requires providing key of the mass every time, which is not held in the system.
    
    Instead, a simple index integer is provided to return a mass particle in the system.

--- tmp.py
+++ tmp.py
@@ -30,13 +30,13 @@ class Particle:
 class MassSpringSystem:
     def __init__(self):
         print(">> Initiated empty mass-spring system")
-        self.masses = {}
+        self.masses = []
         self.connections = []
 
     def add_mass(self, mass):
         if type(mass) is Particle:
             print(f">> Added mass at {mass.center}")
-            self.masses[mass.id_str] = mass
+            self.masses.append(mass)
         else:
             print(f"Expected Particle class, got {type(mass)}")
         
@@ -44,59 +44,40 @@ class MassSpringSystem:
         # TODO: remove mass dictionary entry
         pass
     
-    def update_mass_location(self, mass, new_location):
-        if type(mass) is Particle:
+    def update_mass_location(self, mass_idx, new_location):
+        if type(mass_idx) is int:
             # TODO: assert mass exists
-            self.masses[mass.id_str].center = new_location
-        if type(mass) is str:
-            self.masses[mass].center = new_location
+            self.masses[mass_idx].center = new_location 
         else:
-            print(">> Please provide a valid mass id string or a Particle object.")
+            print(">> Please provide a valid mass index as type int.")
     
-    def connect_masses(self, mass_first : Particle, mass_second : Particle):
+    def connect_masses(self, first_mass_idx : int, second_mass_idx : int):
         # TODO: check if both masses are in the system
-        assert mass_first.id_str != mass_second.id_str, f"Cannot connect particle to itself."
-        assert type(mass_first) == Particle and type(mass_second) == Particle
+        assert type(first_mass_idx) == int and type(second_mass_idx) == int
+        assert first_mass_idx != second_mass_idx, f"Cannot connect particle to itself."
         # TODO: check if connection already exist (m1,m2) or (m2,m1)
         
-        self.connections.append((mass_first.id_str, mass_second.id_str))
+        self.connections.append((first_mass_idx, second_mass_idx))
         return
     
     def disconnect_masses(self, mass_first : Particle, mass_second : Particle):
-        # TODO: remove dictionary entries
-        assert type(mass_first) == Particle and type(mass_second) == Particle
-        
-        try: 
-            self.connections.remove(mass_first.id_str, mass_second.id_str)
-            print(f">> Connection {mass_first.center} - {mass_second.center} deleted.")
-        except:
-            try:
-                self.connections.remove(mass_second.id_str, mass_first.id_str)
-                print(f">> Connection {mass_second.center} - {mass_first.center} deleted.")
-            except:
-                print(">> Mass connection {mass_second.center} - {mass_first.center} not found.")
-                
-    def get_mass_w_index(self, idx):
-        key_at_index_i = list(self.masses.keys())[idx]
-        return self.masses[key_at_index_i]
+        pass
     
     def get_mass_locations(self):
         # TODO: could we store it dynamically rather than gathering them every time?
         mass_locations = []
-        for key in self.masses:
-            mass_particle = self.masses[key]
-            mass_locations.append(mass_particle.center)
-            
+        for mass_particle in self.masses:
+            mass_locations.append(mass_particle.center)   
         return mass_locations
-            
+    
     def get_system_meshes(self):
         # TODO: We can't afford to create separate mesh every time the system is updated
         # so write a function that updates system mesh coordinates as well
         # or store meshes and update the stored mesh every time something is added,
         # and update mesh in every update in particles...
         meshes = []
-        for key in self.masses:
-            mass_particle = self.masses[key]
+        for mass_particle in self.masses:
+            
             sphere = pv.Sphere(radius=mass_particle.radius,center=mass_particle.center, direction=mass_particle.direction)
             meshes.append(sphere)
              
@@ -116,31 +97,31 @@ plotter.camera_position = 'zy'
 plotter.camera.azimuth = -90
 
 mass_spring_system = MassSpringSystem()
-n_masses = 20
+n_masses = 10
 for i in range(n_masses):
     mass_particle = Particle(coordinate=np.random.rand(3))
     mass_spring_system.add_mass(mass_particle)
     
     if i > 0:
-        prev_particle = mass_spring_system.get_mass_w_index(i-1)
-        mass_spring_system.connect_masses(prev_particle, mass_particle)
-
+        mass_spring_system.connect_masses(i-1, i)
 
 system_meshes = mass_spring_system.get_system_meshes()
 system_actors = []
 for mesh in system_meshes:
     actor = plotter.add_mesh(mesh)
     system_actors.append(actor)
+    # TODO separate mass and connection meshes, to make springs not pickable etc.
+    # TODO: Allow render lines as tube
+    # TODO: for the connections, we can implement a zigzag mesh but that's more complicated
     
 plotter.enable_mesh_picking()
 
 def callback(step):
     actor.position = [step / 100.0, step / 100.0, 0]
 
-plotter.add_timer_event(max_steps=200, duration=500, callback=callback)
+plotter.add_timer_event(max_steps=50, duration=100, callback=callback)
 cpos = [(0.0, 0.0, 10.0), (0.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
 plotter.show(cpos=cpos)
-
 
 """
 plotter.show()
