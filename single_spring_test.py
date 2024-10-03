cbe25df8a0ac8c05edab7a1e9d14867b2b6763dc
bartuakyurek <bartuakyurek@outlook.com>
2024-10-03 12:51:37 +0300

    Remove unused comments
    Remove the comments no longer needed.

--- single_spring_test.py
+++ single_spring_test.py
@@ -10,7 +10,6 @@ import pyvista as pv
 
 from mass_spring import MassSpringSystem
         
-
 # -------------------------------- MAIN ---------------------------------------
 # -----------------------------------------------------------------------------
 RENDER = True
@@ -33,45 +32,12 @@ initial_mass_locations = mass_spring_system.get_mass_locations()
 mass_point_cloud = pv.PolyData(initial_mass_locations)
 _ = plotter.add_mesh(mass_point_cloud, render_points_as_spheres=True,
                  show_vertices=True)
-# Affine transform widget duplicate masses, which is not intentional
-#plotter.add_affine_transform_widget(mass_actor)
-
+ 
 # Add springs connections actors in between to PyVista Plotter
 spring_meshes = mass_spring_system.get_spring_meshes()
 for spring_mesh in spring_meshes:
     plotter.add_mesh(spring_mesh)
-    
-"""
-#plotter.open_gif("./results/sample.gif")
-plotter.open_movie("./results/sample.mp4")
-plotter.write_frame()
-
-n_frames = 50
-for frame in range(n_frames-1):
-        
-    if frame == 0:
-        SELECTED_MASS = 1 
-        mass_spring_system.translate_mass(SELECTED_MASS, np.array([0.1,0.3,0.4]))
-      
-    mass_spring_system.simulate()
-    cur_mass_locations = mass_spring_system.get_mass_locations()
-    mass_point_cloud.points = cur_mass_locations
-       
-    # Step 3 - Update the renderd connections based on new locations
-    for i, mass_idx_tuple in enumerate(mass_spring_system.connections):
-        spring_meshes[i].points[0] = cur_mass_locations[mass_idx_tuple[0]]
-        spring_meshes[i].points[1] = cur_mass_locations[mass_idx_tuple[1]]
-    
-    plotter.add_text(f"Iteration: {frame}", name='time-label')
-    plotter.write_frame()
-
-# Closes and finalizes movie
-print(">> Plotter closed.")
-plotter.close()
-plotter.deep_clean()
-
-
-"""
+     
 def callback(step):
     
     # Step 1 - Apply forces (if any) and simulate
@@ -79,7 +45,7 @@ def callback(step):
         print(">> Simulation started.")
         print(f">> {step} Force applied.")
         SELECTED_MASS = 1 
-        mass_spring_system.translate_mass(SELECTED_MASS, np.array([0.0,0.0,-0.2]))
+        mass_spring_system.translate_mass(SELECTED_MASS, np.array([0.0,0.3,-0.2]))
         
     if ((step+1) % 50) == 0:
         print(">> Step ", step)
@@ -103,12 +69,8 @@ plotter.add_timer_event(max_steps=n_simulation_steps, duration=dt_milliseconds, 
 
 plotter.enable_mesh_picking(left_clicking=True)#, pickable_window=False)
 plotter.camera_position = 'zy'
-#plotter.camera.azimuth = -90
-#cam_pos = [(0.0, 0.0, 1.0), (0.0, 0.0, 0.0), (0.0, 0.0, 0.0)]
-plotter.show()#(cpos=cam_pos)
 
-
-
+plotter.show()
 
 
 
