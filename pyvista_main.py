0a5a61ff4686a8d26101532188909fab29a0cddd
bartuakyurek <bartuakyurek@outlook.com>
2024-10-03 13:20:37 +0300

    Add mass-spring simulation
    Add mass-spring simulation steps to SMPL rigid motion. They're not attached yet, it's just the general template and code I borrowed from single spring tests of previous commits.

--- pyvista_main.py
+++ pyvista_main.py
@@ -6,14 +6,18 @@ Created on Mon Sep 30 09:51:49 2024
 @author: bartu
 """
 
-import open3d as o3d
-
-import os
-import numpy as np
 import torch
+import numpy as np
+import pyvista as pv
 
 from smpl_torch_batch import SMPLModel
 from skeleton_data import get_smpl_skeleton
+
+from mass_spring import MassSpringSystem
+from pyvista_render_tools import add_skeleton, add_mesh
+# ---------------------------------------------------------------------------- 
+# Load SMPL animation file and get the mesh and associated rig data
+# ---------------------------------------------------------------------------- 
 
 training_data = torch.load('./data/50004_dataset.pt')
 data_loader = torch.utils.data.DataLoader(training_data, batch_size=1, shuffle=False)
@@ -34,39 +38,65 @@ V = smpl_verts.detach().cpu().numpy()
 J = joints.detach().cpu().numpy()
 n_frames, n_verts, n_dims = target_verts.shape
 
-# ---------------------------------------------------------------------------- 
-import numpy as np
-import pyvista as pv
-
-from pyvista_render_tools import add_skeleton, add_mesh
-
-
-## TODO: Run spring simulation here and save the mass locations for rendering later
-
-
-
-
-## TODO: Render the baked simulation using Pyvista
-## ----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Create a plotter object and set the scalars to the Z height
-
+# -----------------------------------------------------------------------------
 RENDER = True
 plotter = pv.Plotter(notebook=False, off_screen=not RENDER)
 plotter.camera_position = 'zy'
 plotter.camera.azimuth = -90
 
 skel_mesh = add_skeleton(plotter, J[0], kintree)
+# -----------------------------------------------------------------------------
+# Initiate mass-spring system container to be attached to animated rig
+# -----------------------------------------------------------------------------
+dt = 1. / 24
+mass_spring_system = MassSpringSystem(dt)
 
-# Open a gif
+# Add masses to container and connect them, and fixate some of them.
+n_masses =  2
+mass = 10
+mass_spring_system.add_mass(mass_coordinate=np.array([0,0,0]), mass=mass)
+mass_spring_system.add_mass(mass_coordinate=np.array([0,0,1.0]), mass=mass)
+mass_spring_system.connect_masses(0, 1)
+mass_spring_system.fix_mass(0)
+    
+# Add masses with their initial locations to PyVista Plotter
+initial_mass_locations = mass_spring_system.get_mass_locations()
+mass_point_cloud = pv.PolyData(initial_mass_locations)
+_ = plotter.add_mesh(mass_point_cloud, render_points_as_spheres=True,
+                 show_vertices=True)
+ 
+# Add springs connections actors in between to PyVista Plotter
+spring_meshes = mass_spring_system.get_spring_meshes()
+for spring_mesh in spring_meshes:
+    plotter.add_mesh(spring_mesh)
+
+# -----------------------------------------------------------------------------
+# Run the mass-spring simulation based on rig motion
+# -----------------------------------------------------------------------------
+n_frames = 200 
+for frame in range(n_frames):
+    pass
+    mass_spring_system.simulate()
+    
+    # Step 2 - Get current mass positions and update rendered particles
+    cur_mass_locations = mass_spring_system.get_mass_locations()
+    mass_point_cloud.points = cur_mass_locations 
+    
+    # Step 3 - Update the renderd connections based on new locations
+    for i, mass_idx_tuple in enumerate(mass_spring_system.connections):
+        spring_meshes[i].points[0] = cur_mass_locations[mass_idx_tuple[0]]
+        spring_meshes[i].points[1] = cur_mass_locations[mass_idx_tuple[1]]
+
+# -----------------------------------------------------------------------------
+# Render the baked simulation using Pyvista
+# -----------------------------------------------------------------------------
 plotter.open_movie("./results/smpl-skeleton.mp4")
 
-n_frames = 200 
 n_repeats = 10
 for _ in range(n_repeats):
     for frame in range(n_frames-1):
-        # TODO: Reset particle system
-        
-        # TODO: Update particle system
         
         # TODO: Update mesh points
         skel_mesh.points = J[frame]
