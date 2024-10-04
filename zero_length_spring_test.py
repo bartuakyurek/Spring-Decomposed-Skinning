d0d6ae45ccef93830d55a9830a0c2006edde5cce
bartuakyurek <bartuakyurek@outlook.com>
2024-10-04 11:55:38 +0300

    Create zero_length_spring_test.py
    To test spring simulation, create zero-length springs and see if it is stable.

---
+++ zero_length_spring_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 06:06:11 2024
+
+@author: bartu
+"""
+import pyvista as pv
+from mass_spring import MassSpringSystem
+        
+# -------------------------------- MAIN ---------------------------------------
+# -----------------------------------------------------------------------------
+# Create zero-length spring for this test.
+# -----------------------------------------------------------------------------
+
+
+# Fix one of the mass
+
+# Apply rigid transformation (rotation, translation) to fixed mass
+
+# Simulate
+
+# Render
