cf173b009344151a5ff3591271446a6d57151caf
bartuakyurek <bartuakyurek@outlook.com>
2024-10-07 08:50:49 +0300

    Add note
    Add some to-do notes for the project globals.

--- global_vars.py
+++ global_vars.py
@@ -5,6 +5,11 @@ Created on Fri Sep 27 13:36:50 2024
 
 @author: bartu
 """
-
+# TODO: Why space dims are _underscored_ but not homo_coords?
+#       keep the naming convention consistent by changing to either one.
 _SPACE_DIMS_ = 3    # Space dimensions either 2 or 3
-HOMO_COORD = False # Adds 1 to the transformation dimensions if True
+HOMO_COORD = False  # Adds 1 to the transformation dimensions if True
+
+# TODO: Add data paths
+DEBUG = True        # WARNING: This is not used yet
+
