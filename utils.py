afb3c061396448f33be00dc0d38bbe800a3068f7
bartuakyurek <bartuakyurek@outlook.com>
2024-10-09 11:20:54 +0300

    Remove torch
    Torch tests fail for this function, removed the torch support for now.

--- utils.py
+++ utils.py
@@ -14,30 +14,26 @@ def batch_axsang_to_quats(rot):
     
     Parameters
     ----------
-    rot : torch.Tensor or np.ndarray
+    rot : np.ndarray
         axis-angle rotation vector of shape (batch, 3).
 
     Returns
     -------
-    torch.Tensor or np.ndarray
+    np.ndarray
         quaternions representing the provided axis-angle rotations.
 
     """
     assert rot.shape[1] == 3
-
+    assert type(rot) == np.ndarray
+    
     roll = rot[:, 0] / 2.
     pitch = rot[:, 1] / 2.
     yaw = rot[:, 2] / 2.
     
-    if type(rot) == np.ndarray:
-        sin = np.sin
-        cos = np.cos
-        stack = np.stack
-    else: 
-        sin = torch.sin
-        cos = torch.cos
-        stack = torch.stack
-        
+    sin = np.sin
+    cos = np.cos
+    stack = np.stack
+   
     qx = sin(roll) * cos(pitch) * cos(yaw)
     qy = cos(roll) * sin(pitch) * cos(yaw)
     qz = cos(roll) * cos(pitch) * sin(yaw)
