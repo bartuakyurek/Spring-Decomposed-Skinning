Fix rotation multiplication
It turns out our way of multiplying quaternions caused problems because and identity quaternion is [0 0 0 1] but if we directly multiply this numpy array with another numpy array quaternion, we just get identity for all rotations.

So as a solution, use scipy library's rotations to handle quaternion mulitplication.

We can further clean up the code by reducing the number of quat to Rotation conversions we do.

tests/test_forward_kinematics.py
@@ -13,8 +13,10 @@ import igl
 import __init__
 import numpy as np
 import pyvista as pv
+
 from pyvista_render_tools import add_skeleton
 from skeleton import Skeleton
+from global_vars import VERBOSE
 
 joint_locations = np.array([
                             [1., 1., 0.],
@@ -47,12 +49,14 @@ line_segments = np.reshape(np.arange(0, 2*(n_bones-1)), (n_bones-1, 2))
 
 print("Rest bone locations:")
 for i in range(n_bones):
-    print(rest_bone_locations[2*i], "-->",rest_bone_locations[2*i+1] )
+    begin = np.round(rest_bone_locations[2*i], 3)
+    end = np.round(rest_bone_locations[2*i+1], 3)
+    print(begin, "-->", end)
 
 pose = np.array(
                 [
-                 [0.,0. ,0.],
-                 [30.,0.,0.],
+                 [90.,0. ,0.],
+                 [0.,0.,0.],
                  [0.,0. ,0.],
                 ]
                 )
@@ -61,6 +65,8 @@ posed_bones = test_skeleton.pose_bones(pose, degrees=True, exclude_root=EXCLUDE_
           
 print("Posed bone locations:")
 for i in range(n_bones):
-    print(posed_bones[2*i], "-->",posed_bones[2*i+1] )
+    begin = np.round(posed_bones[2*i], 3)
+    end = np.round(posed_bones[2*i+1], 3)
+    print(begin, "-->", end)
     
     