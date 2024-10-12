d87ec33f14a547fbb2e33b80896da62ffef2bb7e
bartuakyurek <bartuakyurek@outlook.com>
2024-10-12 08:22:46 +0300

    Add subroutines
    Add helper functions to create rig and add helper bones. These functions can be relocated to Skeleton class because they're used in every other test to initiate the skeleton correctly.

--- tests/fk_test_spring.py
+++ tests/fk_test_spring.py
@@ -16,31 +16,60 @@ from skeleton import Skeleton
 from pyvista_render_tools import add_skeleton
 from global_vars import IGL_DATA_PATH, RESULT_PATH
 
+# ---------------------------------------------------------------------------- 
+# Declare helper functions
+# ---------------------------------------------------------------------------- 
+def create_skeleton(joint_locations, kintree):
+    test_skeleton = Skeleton(root_vec = joint_locations[0])
+    for edge in kintree:
+         parent_idx, bone_idx = edge
+         test_skeleton.insert_bone(endpoint = joint_locations[bone_idx], 
+                                   parent_idx = parent_idx)   
+    return test_skeleton
+
+def add_helper_bones(test_skeleton, helper_bone_endpoints, helper_bone_parents,
+                     offset_ratio):
+    n_helper = len(helper_bone_parents)
+    for i in range(n_helper):
+        test_skeleton.insert_bone(endpoint = helper_bone_endpoints[i],
+                                  parent_idx = helper_bone_parents[i],
+                                  at_the_tip=False,
+                                  offset_ratio = offset_ratio,
+                                  )
+# ---------------------------------------------------------------------------- 
+# Set skeletal animation data
+# ---------------------------------------------------------------------------- 
 TGF_PATH = IGL_DATA_PATH + "arm.tgf"
 joint_locations, kintree, _, _, _, _ = igl.read_tgf(TGF_PATH)
 
-# ---------------------------------------------------------------------------- 
-# Create skeleton based on loaded data
-# ---------------------------------------------------------------------------- 
-# TODO: if you always create root_vec based on your first data, why don't you 
-#       include it in the insert_bone phase to make it easier to read?
-test_skeleton = Skeleton(root_vec = joint_locations[0])
-for edge in kintree:
-     parent_idx, bone_idx = edge
-     test_skeleton.insert_bone(endpoint = joint_locations[bone_idx], 
-                               parent_idx = parent_idx)
-
 helper_bone_endpoints = np.array([ joint_locations[2] + [0.0, 0.2, 0.0] ])
 helper_bone_parents = [2]
-n_helper = len(helper_bone_parents)
 
-for i in range(n_helper):
-    test_skeleton.insert_bone(endpoint = helper_bone_endpoints[i],
-                              parent_idx = helper_bone_parents[i],
-                              at_the_tip=False,
-                              offset_ratio = 1.0,
-                              #startpoint = test_skeleton.bones[2].end_location-test_skeleton.bones[2].start_location
-                              )
+pose = np.array([
+                [
+                 [0.,0.,0.],
+                 [0.,0.,0.],
+                 [0., 0., 0.],
+                 [0.,0.,0.],
+                 [0.,0.,0.],
+                 [0.,0.,0.],
+                ],
+                [
+                 [0.,0.,0.],
+                 [0.,0.,0.],
+                 [0., 20., 0.],
+                 [0.,0.,0.],
+                 [0.,0.,0.],
+                 [0.,0.,0.],
+                ]
+                ])
+
+# ---------------------------------------------------------------------------- 
+# Create rig and set helper bones
+# ---------------------------------------------------------------------------- 
+
+test_skeleton = create_skeleton(joint_locations, kintree)
+add_helper_bones(test_skeleton, helper_bone_endpoints, helper_bone_parents, offset_ratio=0.5)
 
 # ---------------------------------------------------------------------------- 
 # Create plotter 
@@ -57,51 +86,18 @@ EXCLUDE_ROOT = True
 n_bones = len(test_skeleton.bones)
 rest_bone_locations = test_skeleton.get_rest_bone_locations(exclude_root=EXCLUDE_ROOT)
 line_segments = np.reshape(np.arange(0, 2*(n_bones-1)), (n_bones-1, 2))
+# TODO: rename get_rest_bone_locations() to get_rest_bones() that will also return
+# line_segments based on exclude_root variable
+# (note that you need to re-run other skeleton tests)
 
 skel_mesh = add_skeleton(plotter, rest_bone_locations, line_segments)
 plotter.open_movie(RESULT_PATH + "/igl-skeleton.mp4")
 
 n_repeats = 20
 n_frames = 2
-pose = np.array([
-                [
-                 [0.,0.,0.],
-                 [0.,0.,0.],
-                 [0., 0., 0.],
-                 [0.,0.,0.],
-                 [0.,0.,0.],
-                 [0.,0.,0.],
-                ],
-                [
-                 [0.,0.,0.],
-                 [0.,0.,0.],
-                 [0., 0., 0.],
-                 [0.,0.,0.],
-                 [0.,0.,0.],
-                 [0.,0.,0.],
-                ]
-                ])
-"""
-trans = np.array([
-                [
-                 [0.,0.,0.],
-                 [0.,0.,0.],
-                 [0., 0., 0.],
-                 [0.,0.,0.],
-                 [0.,0.,0.],
-                ],
-                [
-                 [0.0,0.,0.],
-                 [0.,0.,0.],
-                 [0., 0., 0.],
-                 [0.,0.,0.0],
-                 [0.,0.,0.],
-                ]
-                ])
-"""
 for _ in range(n_repeats):
     for frame in range(n_frames):
-        for _ in range(24):
+        for _ in range(24 * 3):
             theta = pose[frame]
             #t = trans[frame]
             posed_bone_locations = test_skeleton.pose_bones(theta, degrees=True, exclude_root=EXCLUDE_ROOT)
