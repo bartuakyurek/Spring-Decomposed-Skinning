a67f67116f73983b130b0af34029c04919115f06
bartuakyurek <bartuakyurek@outlook.com>
2024-10-20 10:31:02 +0300

    Comment unused imports
    Not sure if this is the correct way but it turns out that we don't need these. I'll delete them altogether once I can verify it.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,10 +6,18 @@ Created on Tue Oct  1 07:31:12 2024
 @author: bartu
 """
 
+"""
 import os
 import sys
+
 
 module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
     
+igl_data_path = os.path.abspath(os.path.join('../data/igl_data/'))
+if igl_data_path not in sys.path:
+    sys.path.append(igl_data_path)
+    
+    
+"""
