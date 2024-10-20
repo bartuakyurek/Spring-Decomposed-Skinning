c96915b9874eca6479a1d61fffaaff723c8b1f8c
bartuakyurek <bartuakyurek@outlook.com>
2024-10-20 10:52:13 +0300

    Fix import
    It appears that this line of code is required to append the module path to system path once I refreshed the IDE.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,7 @@ Created on Tue Oct  1 07:31:12 2024
 @author: bartu
 """
 
-"""
+
 import os
 import sys
 
@@ -15,9 +15,3 @@ module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
     
-igl_data_path = os.path.abspath(os.path.join('../data/igl_data/'))
-if igl_data_path not in sys.path:
-    sys.path.append(igl_data_path)
-    
-    
-"""
