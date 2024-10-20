61ffcb1880e6351edd3b2c7bfde3a80e0d720fd4
bartuakyurek <bartuakyurek@outlook.com>
2024-10-20 08:29:33 +0300

    Enable dragging
    Now we can drag the masses around. However, the sphere visuals aren't following the mouse coordinate fully. It should be addressed next.

--- demo/vispy_sample.py
+++ demo/vispy_sample.py
@@ -25,6 +25,7 @@ class MassVisual(scene.visuals.Compound):
                  center, 
                  size=10.0, 
                  selectable=True,
+                 editable=True,
                  on_select_callback=None,
                  callback_argument=None,
                  *args, **kwargs): 
@@ -38,45 +39,72 @@ class MassVisual(scene.visuals.Compound):
         if center.shape == (3,):
             center = np.reshape(center, (1,-1))
 
+        self.size = size
         self.center = center
-        self.default_color =  Color("orange")
-        self.select_color = Color("yellow")
+        self.drag_reference = np.zeros((1,3))  # 3D World coordinates
+
+        self.DEFAULT_COLOR =  Color("orange")
+        self.SELECT_COLOR = Color("yellow")
+        self.color = self.DEFAULT_COLOR
+
         self.marker = scene.visuals.Markers(
-                                            pos = self.center,
-                                            spherical=True,
-                                            size=size,
-                                            antialias=0,
-                                            face_color=self.default_color,
-                                            parent=self
+                                            pos        = self.center,
+                                            spherical  = True,
+                                            size       = self.size,
+                                            antialias  = 0,
+                                            face_color = self.DEFAULT_COLOR,
+                                            parent     = self,
                                             #*args, **kwargs
                                             )
         # We need to set interactive True to enable picking.
         self.marker.interactive = True
-        
+        self.editable = editable
         self._selectable = selectable
         self._on_select_callback = on_select_callback
         self._callback_argument = callback_argument
 
         self.freeze()
 
+    def set_data(self, center=None, size=None, color=None):
+        if center is not None:
+            self.center = center
+        if size is not None:
+            self.size = size
+        if color is not None:
+            self.color = color
+        # Note that you need to provide position data for set_data to work.
+        # Otherwise setting only the colors does not work. 
+        self.marker.set_data(pos=self.center, 
+                             face_color=self.color, 
+                             size=self.size)
+
     def select(self):
         print(">> INFO: Select() called...")
         if self._selectable:
             print(">> INFO: Updating selected colors...")
-            # Note that you need to provide position data for set_data to work.
-            # Otherwise setting only the colors does not work. 
-            self.marker.set_data(pos=self.center, face_color=self.select_color)
+            self.set_data(color=self.SELECT_COLOR)
     
             if self._on_select_callback is not None:
                 self._on_select_callback(self._callback_argument)
+        return
     
     def deselect(self):
         print(">> INFO: Deselect() called...")
         if self._selectable:
             print(">> INFO: Updating deselect colors...")
-            # Note that you need to provide position data for set_data to work.
-            self.marker.set_data(pos=self.center, face_color=self.default_color)
+            self.set_data(color=self.DEFAULT_COLOR)
         return
+    
+    def start_move(self, mouse_pos):
+        self.drag_reference = mouse_pos[0:3] - self.center 
+        print(self.drag_reference)
+
+    def move(self, mouse_pos):
+        # TODO: translate the fixed mass ? 
+        if self.editable:
+            shift = mouse_pos[0:3] - self.drag_reference
+            self.set_data(center=shift)
+
 
 class Canvas(scene.SceneCanvas):
     """ A simple test canvas for drawing demo """
@@ -95,6 +123,7 @@ class Canvas(scene.SceneCanvas):
         self.view.camera.set_range(x=[-5, 5])
         
         self.selected_object = None
+        self.mouse_start_pos = [0, 0] # Mouse pixel coordinates on the 2D screen
         self.objects = []
         self.freeze()
 
@@ -106,13 +135,12 @@ class Canvas(scene.SceneCanvas):
     
     def on_mouse_press(self, event):
         
-        tr = self.scene.node_transform(self.view.scene)
-        pos = tr.map(event.pos)
-
         self.view.interactive = False
         selected = self.visual_at(event.pos)     
         self.view.interactive = True
 
+        tr = self.scene.node_transform(self.view.scene)
+        pos = tr.map(event.pos)
         # Deselect previously selected
         if self.selected_object is not None:
             self.selected_object.deselect()
@@ -121,40 +149,40 @@ class Canvas(scene.SceneCanvas):
         # Left click
         if event.button == 1:
             if selected is not None:
-                print(">> WARNING: Implement what happens after selection")
-                # update transform to selected object
                 self.selected_object = selected.parent
-                
-                tr = self.scene.node_transform(self.selected_object)
-                pos = tr.map(event.pos)
-
                 self.selected_object.select()
-                self.update()
+
+                self.selected_object.start_move(pos)
+                self.mouse_start_pos = event.pos
             else:
                 print(">> INFO: You clicked on empty space.")
         # Right click
         if event.button == 2: 
             print(">> WARNING: Right click doesn't do anything.")
 
-    """
+
     def on_mouse_move(self, event):
 
-        if event.button == 1:
+        if event.button == 1: # If clicked
             if self.selected_object is not None:
                 self.view.camera._viewbox.events.mouse_move.disconnect(
                     self.view.camera.viewbox_mouse_event)
                 
                 # update transform to selected object
-                print(">> Implement on_mouse_move...")
+               
                 tr = self.scene.node_transform(self.selected_object)
                 pos = tr.map(event.pos)
-                #self.selected_object.move(pos[0:2])
+                #trail = event.trail()
+                #print(event.pos.shape)
+                #print(event.trail(), "..")
+                self.selected_object.move(pos)
             else:
                 self.view.camera._viewbox.events.mouse_move.connect(
                     self.view.camera.viewbox_mouse_event)
+                
         else:
             pass
-    """
+  
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
 
