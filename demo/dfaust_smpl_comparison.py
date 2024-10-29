7d136cf2fe85a52f194f806689740f040bc8f9a0
bartuakyurek <bartuakyurek@outlook.com>
2024-10-29 18:01:19 +0300

    Render dfaust and smpl animation side by side
    Now everything is ready to add the helper bone and retrieve the new locations.

--- demo/dfaust_smpl_comparison.py
+++ demo/dfaust_smpl_comparison.py
@@ -13,7 +13,7 @@ compare the computed jigglings with the ground truth DFAUST data.
 import numpy as np
 import pyvista as pv
 
-from src.global_vars import subject_ids, pose_ids
+from src.global_vars import subject_ids, pose_ids, RESULT_PATH
 from src.data.skeleton_data import get_smpl_skeleton
 from src.render.pyvista_render_tools import add_skeleton, add_mesh
 from src.data.smpl_sequence import get_gendered_smpl_model, get_anim_sequence
@@ -30,36 +30,70 @@ V_gt, V_smpl, J, _ = get_anim_sequence(SELECTED_SUBJECT, SELECTED_POSE, smpl_mod
 
 
 # -----------------------------------------------------------------------------
-# Create a plotter object
+# Create a plotter object and add meshes
 # -----------------------------------------------------------------------------
 RENDER = True
-plotter = pv.Plotter(notebook=False, off_screen=not RENDER, shape=(1,2))
+plotter = pv.Plotter(notebook = False, 
+                     off_screen = not RENDER, 
+                     shape = (1,3),
+                     border = False,
+                     window_size = (1504, 1408))
 
+# Data to initialize mesh objects
 initial_J, initial_smpl_V, initial_gt_V = J[0], V_smpl[0], V_gt[0]
-
 
 # Add DFAUST Ground Truth Mesh
 plotter.subplot(0, 0)
 dfaust_mesh = add_mesh(plotter, initial_gt_V, F, opacity=1.0)
-plotter.add_text("No Eye-Dome Lighting", font_size=24)
+plotter.add_text("Ground Truth Deformation", font_size=18)
 plotter.camera_position = [[-0.5,  1.5,  5.5],
                            [-0. ,  0.2,  0.3],
                            [ 0. ,  1. , -0.2]]
 
 # Add SMPL Mesh 
 plotter.subplot(0, 1)
-skel_mesh = add_skeleton(plotter, J[0], kintree)
-smpl_mesh = add_mesh(plotter, initial_smpl_V, F, opacity=1.0)
-plotter.add_text("Eye-Dome Lighting", font_size=24)
+rigid_skel_mesh = add_skeleton(plotter, J[0], kintree)
+rigid_smpl_mesh = add_mesh(plotter, initial_smpl_V, F, opacity=0.8)
+plotter.add_text("SMPL Rigid Deformation", font_size=18)
 plotter.camera_position = [[-0.5,  1.5,  5.5],
                            [-0. ,  0.2,  0.3],
                            [ 0. ,  1. , -0.2]]
 
+# -----------------------------------------------------------------------------
+# Setup a helper rig and simulate 
+# -----------------------------------------------------------------------------
 
-# Show plot
-def foo(value):
-    print("===")
-    print(np.round(plotter.camera_position, 1))
-    print("---")
-plotter.add_slider_widget(foo, [5, 100], title='Resolution')
-plotter.show()
+# TODO: Precomputation here...
+# Note that we could do the computation inside the render loop
+# TODO: Report timing
+J_dyn = J.copy()
+V_dyn = V_smpl.copy()
+
+# Add SMPL mesh to be jiggled
+plotter.subplot(0, 2)
+dyn_skel_mesh = add_skeleton(plotter, J_dyn[0], kintree)
+dyn_smpl_mesh = add_mesh(plotter, initial_smpl_V, F, opacity=0.8)
+plotter.add_text("Spring Deformation", font_size=18)
+plotter.camera_position = [[-0.5,  1.5,  5.5],
+                           [-0. ,  0.2,  0.3],
+                           [ 0. ,  1. , -0.2]]
+
+# -----------------------------------------------------------------------------
+# Render and save results
+# -----------------------------------------------------------------------------
+    
+result_fname = "dfaust_comparison"
+plotter.open_movie(RESULT_PATH + f"{result_fname}.mp4")
+
+n_frames = V_smpl.shape[0]
+for frame in range(n_frames):
+    rigid_skel_mesh.points = J[frame]   # Update mesh points in the renderer.
+    dyn_skel_mesh.points = J_dyn[frame] # TODO: update it!
+    
+    dfaust_mesh.points = V_gt[frame]
+    rigid_smpl_mesh.points = V_smpl[frame]
+    dyn_smpl_mesh.points = V_dyn[frame]
+    
+    plotter.write_frame()               # Write a frame. This triggers a render.
+    
+plotter.close()
