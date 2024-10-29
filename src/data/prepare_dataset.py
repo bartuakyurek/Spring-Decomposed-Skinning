1447968906a8b7cdb34c5d76086eec7681648575
bartuakyurek <bartuakyurek@outlook.com>
2024-10-29 09:06:49 +0300

    Extract function for retrieving the bpts data
    To be used in other cases as well, extract this function that retrieves body, pose, trans data for SMPL parameters.

--- src/data/prepare_dataset.py
+++ src/data/prepare_dataset.py
@@ -14,12 +14,15 @@ and saves a dataset that includes [beta, pose, trans] for every instance.
 import os
 import glob
 import h5py
-import numpy as np
 import torch
+import numpy as np
 from torch import nn
-from smpl_torch_batch import SMPLModel
 from time import time
 from tqdm import tqdm
+
+import sys
+sys.path.append('../') # For parent directory packages
+from models.smpl_torch_batch import SMPLModel
 
 
 sids = ['50004', '50020', '50021', '50022', '50025',
@@ -33,14 +36,31 @@ pids = ['hips', 'knees', 'light_hopping_stiff', 'light_hopping_loose',
 femaleids = sids[:5]
 maleids = sids[5:]
 
-regis_dir = './data/dyna/dyna_dataset_f.h5'
-data_dir = './data/DFaust_67'
+regis_dir = '../../data/dyna/dyna_dataset_f.h5'
+data_dir = '../../data/DFaust_67'
 
 f = h5py.File(regis_dir, 'r')
 
 comp_device = torch.device('cpu')
-smplmodel = SMPLModel(model_path='./body_models/smpl/female/model.pkl',device=comp_device)
+smplmodel = SMPLModel(model_path='../models/body_models/smpl/female/model.pkl',device=comp_device)
 
+def get_instance_data(sidpid):
+    verts = f[sidpid][()].transpose([2, 0, 1])
+    verts = torch.Tensor(verts).type(torch.float64)
+    bdata = np.load(data_fname)
+    betas = torch.Tensor(bdata['betas'][:10][np.newaxis]).squeeze().type(torch.float64)
+    pose_body = torch.Tensor(bdata['poses'][:, 3:72]).squeeze().type(torch.float64)
+    pose_body = torch.cat((torch.zeros(pose_body.shape[0], 3).type(torch.float64),pose_body),1)
+    
+    trans = torch.Tensor(bdata['trans']).type(torch.float64)
+    num_frame = pose_body.shape[0]
+    betas = betas.repeat(num_frame,1)
+    
+    smpl_vert, _ = smplmodel(betas, pose_body, trans) 
+    translation = torch.mean(verts - smpl_vert, 1).view(num_frame,1,3)
+    target_verts = verts - translation
+    assert target_verts.shape == verts.shape
+    return betas, pose_body, trans, target_verts
 
 dataset = []
 for femaleid in femaleids:
@@ -49,30 +69,14 @@ for femaleid in femaleids:
     
     for data_fname in tqdm(data_fnames):
         
-        #base_name = os.path.basename(os.path.normpath(data_fname)) # Base name is "sid*_pid*_poses.npz"
-        #sidpid = base_name[:-10] # Remove '_poses.npz' part from the file name
-        sidpid = data_fname[23:-10]
-        verts = f[sidpid][()].transpose([2, 0, 1])
-        verts = torch.Tensor(verts).type(torch.float64)
-        bdata = np.load(data_fname)
-        betas = torch.Tensor(bdata['betas'][:10][np.newaxis]).squeeze().type(torch.float64)
-        pose_body = torch.Tensor(bdata['poses'][:, 3:72]).squeeze().type(torch.float64)
-        pose_body = torch.cat((torch.zeros(pose_body.shape[0], 3).type(torch.float64),pose_body),1)
+        sidpid = os.path.basename(data_fname)[:-len("_poses.npz")] 
+        betas, pose_body, trans, target_verts = get_instance_data(sidpid)
         
-        if pose_body.shape[0]-verts.shape[0] != 0:
+        if pose_body.shape[0] != target_verts.shape[0]:
             print(">> Found erronous data, skipping...")
             continue
-        
-        trans = torch.Tensor(bdata['trans']).type(torch.float64)
-        num_frame = pose_body.shape[0]
-        betas = betas.repeat(num_frame,1)
-        
-        smpl_vert, _ = smplmodel(betas, pose_body, trans) 
-        translation = torch.mean(verts - smpl_vert, 1).view(num_frame,1,3)
-        tar_bs = verts - translation
-        
         bpts = torch.cat((betas,pose_body,trans),1)
-        dataset.append((bpts,tar_bs))
+        dataset.append((bpts,target_verts))
         
     
 torch.save(dataset, 'female_bpts2dbs.pt')
