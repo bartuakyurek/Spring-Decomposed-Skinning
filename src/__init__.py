6bce6a6c7e4982df0952a29e2dbb8cc680c0ffe1
bartuakyurek <bartuakyurek@outlook.com>
2024-10-29 09:05:25 +0300

    Delete comments in __init__

--- src/__init__.py
+++ src/__init__.py
@@ -5,14 +5,3 @@ Created on Tue Oct  1 07:31:12 2024
 
 @author: bartu
 """
-"""
-import skeleton
-import skinning
-import mass_spring
-import helper_handler
-import optimal_rigid_motion
-
-import utils
-import render
-import models 
-"""
--- src/render/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  1 07:31:12 2024
-
-@author: bartu
-"""
-
-#import pyvista_render_tools
