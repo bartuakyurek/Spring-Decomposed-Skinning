ee83c31e62fcca2a5f7e65e2ca8f1d3d85a7ff8a
bartuakyurek <bartuakyurek@outlook.com>
2024-11-09 15:45:44 +0300

    Adjust setting for monstera demo
    These settings seems to be nice for the vanilla demo.

--- demo/monstera_demo.py
+++ demo/monstera_demo.py
@@ -20,7 +20,6 @@ from src.utils.linalg_utils import lerp
 from src.kinematics import inverse_kinematics
 from src.helper_handler import HelperBonesHandler
 from src.global_vars import DATA_PATH, RESULT_PATH
-from src.render.pyvista_render_tools import add_skeleton, add_mesh
 from src.skeleton import Skeleton, create_skeleton_from
 from src.render.pyvista_render_tools import (add_mesh, 
                                              add_skeleton, 
@@ -30,7 +29,7 @@ from src.render.pyvista_render_tools import (add_mesh,
 # ----------------------------------------------------------------------------
 # Declare parameters
 # ----------------------------------------------------------------------------
-MODE = "Dynamic" #"Rigid" or "Dynamic" 
+MODE = "Rigid" #"Rigid" or "Dynamic" 
 INTEGRATION = "PBD" # PBD or Euler
 ALGO = "RST" # RST, SVD, T
 NORMALIZE_WEIGHTS = True
@@ -38,31 +37,32 @@ NORMALIZE_WEIGHTS = True
 RENDER_MESH = True
 RENDER_SKEL = False
 RENDER_PHYS_BASED = True
-MATERIAL_METALLIC = 1.0 
-MATERIAL_ROUGHNESS = 0.4
+MATERIAL_METALLIC = 0.2
+MATERIAL_ROUGHNESS = 0.5
 
 FIXED_SCALE = True # Set true if you want the jiggle bone to preserve its length
 POINT_SPRING = True # Set true for less jiggling (point spring at the tip), set False to jiggle the whole bone as a spring.
 EXCLUDE_ROOT = True # Set true in order not to render the invisible root bone (it's attached to origin)
 DEGREES = True # Set true if pose is represented with degrees as Euler angles.
-N_REPEAT = 3
+N_REPEAT = 4
 
 FRAME_RATE = 24 # 24, 30, 60
 TIME_STEP = 1./FRAME_RATE  
-MASS = 0.1
-STIFFNESS = 10.
+MASS = 10.5
+STIFFNESS = 100.
 DAMPING = 1.            
-MASS_DSCALE = 0.8       # Scales mass velocity (Use [0.0, 1.0] range to slow down)
-SPRING_DSCALE = 2.0     # Scales spring forces (increase for more jiggling)
+MASS_DSCALE = 0.4       # Scales mass velocity (Use [0.0, 1.0] range to slow down)
+SPRING_DSCALE = 1.0     # Scales spring forces (increase for more jiggling)
 
 FNAME = "monstera"      # For i/o files
+DATA_PATH = DATA_PATH + FNAME + "/"
 OBJ_PATH = DATA_PATH + FNAME + ".obj"
-MONSTERA_RIG_PATH = "../data/" + FNAME + "_rig_data.npz"
+MONSTERA_RIG_PATH = DATA_PATH + FNAME + "_rig_data.npz"
 
 # ---------------------------------------------------------------------------- 
 # Set skeletal animation data 
 # ---------------------------------------------------------------------------- 
-V_rest, texture_coord, _, F, _, _ =  igl.read_obj(OBJ_PATH)
+V_rest, _, _, F, _, _ =  igl.read_obj(OBJ_PATH)
 keyframe_poses = poses.monstera_rig_pose
 
 with np.load(MONSTERA_RIG_PATH) as data:
@@ -70,7 +70,7 @@ with np.load(MONSTERA_RIG_PATH) as data:
      B = data["joints"] # (n_bones, 2, 3)
      kintree = data["kintree"] # (n_bones, 2)
 
-# ---- Rotate, translate, scale the rig data if needed ---------------
+# ---- Rotate, translate, scale the rig data if needed -------
 
 # Rotate 
 from scipy.spatial.transform import Rotation
@@ -84,12 +84,12 @@ B = B - np.array([0, 0, 0.6])
 # Scale
 B = B * 0.37
 
-# ------------------------------------------------------------
-
+# ---------------------------------------------------------------------------- 
+# Define helper spring bones on the existing skeleton
+# ---------------------------------------------------------------------------- 
 n_bones = len(kintree)
 skeleton = create_skeleton_from(B, kintree)
 helper_idxs = np.array([i for i in range(1, n_bones)])
-
 
 helper_rig = HelperBonesHandler(skeleton, 
                                 helper_idxs,
@@ -115,6 +115,9 @@ plotter.camera.position = [-8.0, 2.0, 0]
 plotter.camera.view_angle = 20 # This works like zoom actually
 plotter.camera.focal_point = (0.0, 0.65, 0.0)
 
+# Add light
+light = pv.Light(position=(0, 1.5, 1.5), light_type='scene light')
+plotter.add_light(light)
 
 # ---------------------------------------------------------------------------- 
 # Add mesh actors
@@ -126,57 +129,70 @@ if RENDER_SKEL:
     skel_mesh = add_skeleton(plotter, rest_bone_locations, line_segments)
 
 if RENDER_MESH: 
-    # Texture coordinates do not work as I expected...
-    #texture_coord = pv.numpy_to_texture(texture_coord)
+    
     mesh, mesh_actor = add_mesh(plotter, V_rest, F, opacity=OPACITY, return_actor=True,
                                 pbr=RENDER_PHYS_BASED, metallic=MATERIAL_METALLIC, roughness=MATERIAL_ROUGHNESS)
+    
+    #texture = pv.read_texture(DATA_PATH + "texture/1.png") 
+    #mesh.active_texture_coordinates = tc
     #mesh.texture_map_to_plane(inplace=True)
-    #mesh_actor.texture = texture_coord
+    #mesh_actor.texture = texture
 
-plotter.open_movie(RESULT_PATH + f"/{FNAME}-{ALGO}.mp4")
+plotter.open_movie(RESULT_PATH + f"/{FNAME}-{ALGO}-{MODE}.mp4")
 n_poses = keyframe_poses.shape[0]
 trans = np.zeros((n_bones+1, 3)) # TODO: remove +1 when you remove root bone issue
-try:
-    for rep in range(N_REPEAT):         # This can be refactored too as it's not related to render
-        for pose_idx in range(n_poses): # Loop keyframes, this could be refactored.
-            for frame_idx in range(FRAME_RATE):
+
+
+# ---------------------------------------------------------------------------------
+# Simulate and save data
+# ---------------------------------------------------------------------------------
+V_anim = []
+J_anim = []
+for rep in range(N_REPEAT):         # This can be refactored too as it's not related to render
+    for pose_idx in range(n_poses): # Loop keyframes, this could be refactored.
+        for frame_idx in range(FRAME_RATE):
                 
-                if pose_idx: # If not the first pose
+            if pose_idx: # If not the first pose
                         theta = lerp(keyframe_poses[pose_idx-1], keyframe_poses[pose_idx], frame_idx/FRAME_RATE)
-                else:        # Lerp with the last pose for boomerang
+            else:        # Lerp with the last pose for boomerang
                         theta = lerp(keyframe_poses[pose_idx], keyframe_poses[-1], frame_idx/FRAME_RATE)
                 
-                posed_locations = skeleton.pose_bones(theta, trans, degrees=DEGREES)
+            posed_locations = skeleton.pose_bones(theta, trans, degrees=DEGREES)
+            abs_rot_quat, abs_trans = skeleton.get_absolute_transformations(theta, trans, degrees=DEGREES)
+            M_rigid = skinning.get_transform_mats_from_quat_rots(abs_trans, abs_rot_quat)[1:] # TODO...
                 
-                abs_rot_quat, abs_trans = skeleton.get_absolute_transformations(theta, trans, degrees=DEGREES)
-                M_rigid = skinning.get_transform_mats_from_quat_rots(abs_trans, abs_rot_quat)[1:] # TODO...
-                
-                if MODE=="Rigid":
-                   skel_mesh_points = posed_locations[2:] # TODO: get rid of root bone convention
-                   if RENDER_MESH: mesh_points = skinning.LBS_from_quat(V_rest, W, abs_rot_quat[1:], abs_trans[1:], use_normalized_weights=NORMALIZE_WEIGHTS) # TODO: get rid of root
-                else:
-                    posed_locations = helper_rig.update_bones(posed_locations) # Update the rigidly posed locations
-                    skel_mesh_points = posed_locations[2:] # TODO: get rid of root bone convention
+            if MODE=="Rigid":
+                skel_mesh_points = posed_locations[2:] # TODO: get rid of root bone convention
+                if RENDER_MESH: mesh_points = skinning.LBS_from_quat(V_rest, W, abs_rot_quat[1:], abs_trans[1:], use_normalized_weights=NORMALIZE_WEIGHTS) # TODO: get rid of root
+            else:
+                posed_locations = helper_rig.update_bones(posed_locations) # Update the rigidly posed locations
+                skel_mesh_points = posed_locations[2:] # TODO: get rid of root bone convention
                    
-                    rest_bone_locations = skeleton.get_rest_bone_locations(exclude_root=False) # TODO: Remove this line from here
-                    M = inverse_kinematics.get_absolute_transformations(rest_bone_locations, posed_locations, return_mat=True, algorithm=ALGO)[1:]  # TODO: get rid of root
+                rest_bone_locations = skeleton.get_rest_bone_locations(exclude_root=False) # TODO: Remove this line from here
+                M = inverse_kinematics.get_absolute_transformations(rest_bone_locations, posed_locations, return_mat=True, algorithm=ALGO)[1:]  # TODO: get rid of root
                     
-                    M_hybrid = M_rigid
-                    M_hybrid[helper_idxs] = M[helper_idxs]
-                    if RENDER_MESH: mesh_points = skinning.LBS_from_mat(V_rest, W, M_hybrid, use_normalized_weights=NORMALIZE_WEIGHTS)
+                M_hybrid = M_rigid
+                M_hybrid[helper_idxs] = M[helper_idxs]
+                if RENDER_MESH: mesh_points = skinning.LBS_from_mat(V_rest, W, M_hybrid, use_normalized_weights=NORMALIZE_WEIGHTS)
                        
-                # Set data for renderer
-                if RENDER_MESH: mesh.points = mesh_points
-                if RENDER_SKEL: skel_mesh.points = skel_mesh_points # Update mesh points in the renderer.
-                plotter.write_frame()               # Write a frame. This triggers a render.
-except AssertionError:
-    print(">>>> Caught assertion, stopping execution...")
-    plotter.close()
-    raise
+            
+            if RENDER_MESH: V_anim.append(mesh_points)
+            if RENDER_SKEL: J_anim.append(skel_mesh_points)
+
     
+
 # ---------------------------------------------------------------------------------
-# Quit the renderer and close the movie.
+# Show computed results
 # ---------------------------------------------------------------------------------
+
+n_frames = max(len(V_anim), len(J_anim))
+for frame in range(n_frames):
+    # Set data for renderer
+    if RENDER_MESH: mesh.points = V_anim[frame]
+    if RENDER_SKEL: skel_mesh.points = J_anim[frame] 
+    plotter.write_frame()   # Write a frame. This triggers a render.
+    
+
 plotter.close()
 plotter.deep_clean()
 
