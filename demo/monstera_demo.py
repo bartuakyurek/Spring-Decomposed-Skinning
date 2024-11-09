034d7dead154c69d71b86258b9fa0f3ddc5367ec
bartuakyurek <bartuakyurek@outlook.com>
2024-11-09 11:35:23 +0300

    Separate spring and rigid transforms
    We were computing RST for all bones but we should only compute it for spring bones so I added a M_hybrid that combines both.

--- demo/monstera_demo.py
+++ demo/monstera_demo.py
@@ -30,8 +30,9 @@ from src.render.pyvista_render_tools import (add_mesh,
 # ----------------------------------------------------------------------------
 # Declare parameters
 # ----------------------------------------------------------------------------
-MODE = "Rigid" #"Rigid" or "Dynamic" 
+MODE = "Dynamic" #"Rigid" or "Dynamic" 
 INTEGRATION = "Euler" # PBD or Euler
+ALGO = "RST" # RST, SVD, T
 FIXED_SCALE = False # Set true if you want the jiggle bone to preserve its length
 POINT_SPRING = False # Set true for less jiggling (point spring at the tip), set False to jiggle the whole bone as a spring.
 EXCLUDE_ROOT = True # Set true in order not to render the invisible root bone (it's attached to origin)
@@ -115,7 +116,7 @@ line_segments = np.reshape(np.arange(0, 2*(n_bones-1)), (n_bones-1, 2))
 skel_mesh = add_skeleton(plotter, rest_bone_locations, line_segments)
 mesh, mesh_actor = add_mesh(plotter, V_rest, F, opacity=OPACITY, return_actor=True)
 
-plotter.open_movie(RESULT_PATH + f"/{FNAME}-m{MASS}-k{STIFFNESS}-kd{DAMPING}-mds{MASS_DSCALE}-sds{SPRING_DSCALE}-fixedscale-{FIXED_SCALE}-pointspring-{POINT_SPRING}.mp4")
+plotter.open_movie(RESULT_PATH + f"/{FNAME}-{ALGO}.mp4")
 n_poses = keyframe_poses.shape[0]
 trans = np.zeros((n_bones+1, 3)) # TODO: remove +1 when you remove root bone issue
 try:
@@ -129,17 +130,23 @@ try:
                         theta = lerp(keyframe_poses[pose_idx], keyframe_poses[-1], frame_idx/FRAME_RATE)
                 
                 posed_locations = skeleton.pose_bones(theta, trans, degrees=DEGREES)
+                
+                abs_rot_quat, abs_trans = skeleton.get_absolute_transformations(theta, trans, degrees=DEGREES)
+                M_rigid = skinning.get_transform_mats_from_quat_rots(abs_trans, abs_rot_quat)[1:] # TODO...
+                
                 if MODE=="Rigid":
                    skel_mesh_points = posed_locations[2:] # TODO: get rid of root bone convention
-                   abs_rot_quat, abs_trans = skeleton.get_absolute_transformations(theta, trans, degrees=DEGREES)
                    mesh_points = skinning.LBS_from_quat(V_rest, W, abs_rot_quat[1:], abs_trans[1:]) # TODO: get rid of root
                 else:
                     posed_locations = helper_rig.update_bones(posed_locations) # Update the rigidly posed locations
                     skel_mesh_points = posed_locations[2:] # TODO: get rid of root bone convention
                    
                     rest_bone_locations = skeleton.get_rest_bone_locations(exclude_root=False) # TODO: Remove this line from here
-                    M = inverse_kinematics.get_absolute_transformations(rest_bone_locations, posed_locations, return_mat=True, algorithm="RST")
-                    mesh_points = skinning.LBS_from_mat(V_rest, W, M[1:]) # TODO: get rid of root
+                    M = inverse_kinematics.get_absolute_transformations(rest_bone_locations, posed_locations, return_mat=True, algorithm=ALGO)[1:]  # TODO: get rid of root
+                    
+                    M_hybrid = M_rigid
+                    M_hybrid[helper_idxs] = M[helper_idxs]
+                    mesh_points = skinning.LBS_from_mat(V_rest, W, M_hybrid)
                        
                 # Set data for renderer
                 mesh.points = mesh_points
