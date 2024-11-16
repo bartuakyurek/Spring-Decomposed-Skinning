e553576dc5c3e5081ffd84b45a996cdb0a128603
bartuakyurek <bartuakyurek@outlook.com>
2024-11-16 14:34:56 +0300

    Convert point handle assert to warning
    The skeleton class actually works the same with point handles because also theoretically they are the same. A point handle is a zero-length bone or vice versa, a bone is composed of two point handles.

--- src/skeleton.py
+++ src/skeleton.py
@@ -365,7 +365,8 @@ class Skeleton():
         
         parent_bone = self.rest_bones[parent_idx]
         new_bone = Bone(endpoint, idx=len(self.rest_bones), parent=parent_bone)
-        assert np.linalg.norm(new_bone.end_location - new_bone.start_location) > 0.0, "Expected inserted bone to have non-zero length. Please use a PointHandle interface for different types of handles."
+        if np.linalg.norm(new_bone.end_location - new_bone.start_location) < 1e-18:
+            print("WARNING: Expected inserted bone to have non-zero length. Please use a PointHandle interface for different types of handles.")
         
         self.rest_bones.append(new_bone)
         self.rest_bones[parent_idx].add_child(new_bone)
